8c44181b15d978a55620b7c431e52e48547633c8
Zaide Kurti <zaiido1920@gmail.com>
2023-01-31 16:42:42 +0100

    Updated MyFooter Component

--- src/components/MyFooter.jsx
+++ src/components/MyFooter.jsx
@@ -2,7 +2,7 @@ const MyFooter = () => {
     return (
         <footer className="footer mt-auto py-3 bg-dark">
             <div className="container">
-                <span className="text-muted"> © 2020 Copyright: EpiBooks</span>
+                <span className="text-muted"> © 2023 Copyright: EpiBooks</span>
             </div>
         </footer>
     );
