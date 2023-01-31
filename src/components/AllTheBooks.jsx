0d1145b4c2ae0af23b4f14eb08adb12d9c65b675
Zaide Kurti <zaiido1920@gmail.com>
2023-01-31 18:41:39 +0100

    Added buttons

--- src/components/AllTheBooks.jsx
+++ src/components/AllTheBooks.jsx
@@ -1,13 +1,29 @@
 import { Component } from "react";
-import { Container, Row, Col, Card, Badge } from "react-bootstrap";
-import books from '../books/history.json'
+import { Container, Button, Row, Col, Card, Badge } from "react-bootstrap";
+import history from '../books/history.json'
+import fantasy from '../books/fantasy.json'
+import horror from '../books/horror.json'
+import romance from '../books/romance.json'
+import scifi from '../books/scifi.json'
 
 class AllTheBooks extends Component {
+    state = {
+        books: history
+    }
+
+    BookGenre(genre) {
+        this.setState({ books: genre })
+    }
     render() {
         return (
             <Container>
+                <Button onClick={() => this.BookGenre(history)} variant="secondary">History</Button>{' '}
+                <Button onClick={() => this.BookGenre(fantasy)} variant="secondary">Fantasy</Button>{' '}
+                <Button onClick={() => this.BookGenre(horror)} variant="secondary">Horror</Button>{' '}
+                <Button onClick={() => this.BookGenre(romance)} variant="secondary">Romance</Button>{' '}
+                <Button onClick={() => this.BookGenre(scifi)} variant="secondary">Scifi</Button>
                 <Row className="my-4">
-                    {books.map((book) => {
+                    {this.state.books.map((book) => {
                         return <Col xs={6} md={4} lg={3} className="my-3" key={book.asin}>
                             <Card>
                                 <Card.Img variant="top" src={book.img} />
